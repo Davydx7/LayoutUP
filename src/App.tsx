f02ab3e5ee2c8dfe94cbf15af8ee0fbfbccd9f4e
David Olufemi <oludave0511@gmail.com>
2023-03-19 19:12:03 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,8 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
+          Edit <code>src/App.tsx
+            dsf</code> and save to test HMR.
         </p>
 
         <p>
